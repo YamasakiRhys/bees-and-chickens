13341ebe88aca359aa09c7c51e8605c7be42f0cf
Rhys_Yamasaki <yamasaki.rhys@gmail.com>
2018-10-08 11:07:21 -0400

    added component for homescreen and bootstrap to index.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeScreenComponent } from './home-screen/home-screen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeScreenComponent
   ],
   imports: [
     BrowserModule
